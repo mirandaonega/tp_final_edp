259b26894c6ee223a17db49d852061f10fd28b9c
itssofiarce <109768938+itssofiarce@users.noreply.github.com>
2022-12-20 19:22:23 -0300

    error ortográfico

--- scripts/substringReplace.sh
+++ scripts/substringReplace.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo ¿Qué subccadenas queres reemplazar en $(basename textos/prueba.txt)?
+echo ¿Qué subcadenas queres reemplazar en $(basename textos/prueba.txt)?
 read -p "Primer subcadena: " CADENA1
 CADENA2=$(echo $CADENA1 | tr '[:upper:][:lower:]' '[:lower:][:upper:]')
 
